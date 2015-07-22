32c952ccc05d2a214932cb82f0b4c93261263af6
David Jaimes <davidjaimest@gmail.com>
2015-07-22 09:12:48 -0500

    Definición básica de la encuesta.
    Se añadió la estructura try / catch.  Se debe continuar con validaciones y demás requerimientos del programa.

--- LAB-Encuesta.cs
+++ LAB-Encuesta.cs
@@ -10,9 +10,39 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            char continuar = "s";
+            byte contador = 0;
             
+            while(continuar == "s"){
+                
+                try{
+                    Console.WriteLine("Bienvenido, ésta es una encuesta para conocer su opinión acerca del proceso de paz en Colombia");    
+                    Console.WriteLine("Digita tu nombre");
+                    string nombre = Console.ReadLine();
+                    Console.WriteLine("Digita tu edad");
+                    byte edad = ConvertToByte(Console.ReadLine());
+                    Console.WriteLine("Digita tu genero masculino: m  femenino: f");
+                    char genero = Console.ReadLine();
+                    Console.WriteLine("Digita tu estrato socio-económico");
+                    byte estrato = ConvertToByte(Console.ReadLine());
+                    
+                    Console.WriteLine("En el proceso de paz que atraviesa Colombia, cree Usted 
+                    que el cese bilateral del fuego se logrará en el 2015 (s/n) ? ");
+                    
+                    contador += 1;
+                    
+                    Console.WriteLine("Desea continuar?");
+                    continuar = Console.ReadLine();
+                }
+                
+                catch(Exception e){
+                    Console.WriteLine(e.Message);
+                }
+                
+                
+            }
+        
             
-  
         }
     }
 }
